c0bfdd7f949c10c85293aaa5d164bd924ab400b7
Vladislav_Strelkov <svisop8@gmail.com>
2021-06-07 23:11:56 +0300

    del old file

--- my-works-examples/Creative-Digital-Agency/js/main.js
+++
@@ -1,4 +0,0 @@
-$('#menu-show').click(function() {
-    $('.header__head_nav div').toggleClass('show');
-    $('.header__title h1').toggleClass('info');
-});
--- my-works-examples/portfolio-new/js/main.js
+++
@@ -1,8 +0,0 @@
-
-  $('.my-works-examples').slick({
-  	arrows: false,
-  	speed: 400,
-  	infinite: false,
-  	fade: true,
-    cssEase: 'linear'
-  });
